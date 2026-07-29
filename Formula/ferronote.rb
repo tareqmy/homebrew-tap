0200639bce0c2b453cf8e4549b17e591e6b8a66d
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2026-07-29 17:21:56 +0600

    Update ferronote to v1.0.6

--- Formula/ferronote.rb
+++ Formula/ferronote.rb
@@ -1,8 +1,8 @@
 class Ferronote < Formula
   desc "Blazing-fast terminal note-taking app inspired by Notational Velocity"
   homepage "https://github.com/tareqmy/ferronote"
-  url "https://github.com/tareqmy/ferronote/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  url "https://github.com/tareqmy/ferronote/archive/refs/tags/v1.0.6.tar.gz"
+  sha256 "41894d7ce1b45979413e2d091c04097da99f231bb6560fde1812dc3a2068782b"
   license "MIT"
 
   depends_on "rust" => :build
